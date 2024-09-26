2cd7943081508b7da484536f1a94037a06d90bdb
Parag Gawde <paraggawde1@gmail.com>
2024-09-26 14:36:11 +0530

    Initial commit

---
+++ src/main/java/com/example/awsdeploy/AwsdeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsdeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsdeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/awsdeploy/AwsdeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsdeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
